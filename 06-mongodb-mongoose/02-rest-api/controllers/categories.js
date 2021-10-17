e0830b9c2a7d40bbdf99a466585abd9cd8910186
dariaselezneva <serafina661@gmail.com>
2021-10-18 02:13:10 +0300

    6.2. mongoDB get requests

--- 06-mongodb-mongoose/02-rest-api/controllers/categories.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/categories.js
@@ -1,3 +1,15 @@
+const Category = require('../models/Category');
+
 module.exports.categoryList = async function categoryList(ctx, next) {
-  ctx.body = {categories: []};
+  const categories = await Category.find({});
+  ctx.status = 200;
+  ctx.body = {
+    categories: categories.map((category) => ({
+      id: category._id,
+      title: category.title,
+      subcategories: category.subcategories.map((subcategory) => ({
+        id: subcategory._id,
+        title: subcategory.title,
+      })),
+    }))};
 };
--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,62 @@
+const Product = require('../models/Product');
+const mongoose = require('mongoose');
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  const productsBySubcategory = await Product.find(subcategory);
+
+  ctx.status = 200;
+  ctx.body = {
+    products: productsBySubcategory.map((product) => ({
+      id: product._id,
+      title: product.title,
+      images: product.images,
+      category: product.category,
+      price: product.price,
+      description: product.description,
+    })),
+  };
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find({});
+  ctx.status = 200;
+  ctx.body = {
+    products: products.map((product) => ({
+      id: product._id,
+      title: product.title,
+      images: product.images,
+      category: product.category,
+      price: product.price,
+      description: product.description,
+    })),
+  };
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
+  const id = ctx.params.id;
+  if (mongoose.Types.ObjectId.isValid(id)) {
+    const productById = await Product.find(id);
+    if (productById) {
+      ctx.status = 200;
+      ctx.body = {
+        product: {
+          id: productById._id,
+          title: productById.title,
+          images: productById.images,
+          category: productById.category,
+          price: productById.price,
+          description: productById.description,
+        },
+      };
+    } else {
+      ctx.status = 404;
+    }
+  } else {
+    ctx.status = 400;
+  }
 };
 
