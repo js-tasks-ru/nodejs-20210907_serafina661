80baf0a0c45e17c8769ba8c8e9a9998c3b4a13d0
dariaselezneva <serafina661@gmail.com>
2021-10-18 01:28:06 +0300

    6.1. mongoDB schemas

--- 06-mongodb-mongoose/01-schema-model/models/Product.js
+++ 06-mongodb-mongoose/01-schema-model/models/Product.js
@@ -2,6 +2,23 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const productSchema = new mongoose.Schema({
+  title: {
+    required: true,
+    type: String,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+  price: {
+    required: true,
+    type: Number,
+  },
+  category: {required: true, type: mongoose.Schema.Types.ObjectId, ref: 'Category'},
+  subcategory: {required: true, type: mongoose.Schema.Types.ObjectId},
+  images: [String],
 });
+
+console.log(productSchema);
 
 module.exports = connection.model('Product', productSchema);
